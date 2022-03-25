f3e21a1d78c5338c72afb04f7ef8c909a80c8df1
s-kuhn <62448107+s-kuhn@users.noreply.github.com>
2022-03-25 17:35:16 +0100

    Update entrypoint.sh

--- elk/fileserver/entrypoint.sh
+++ elk/fileserver/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh-keygen -A && \
 exec /usr/sbin/sshd -D -e "$@" & \
-/usr/local/bin/app -port 8888 -token 123 -upload_limit 100000000 /home/projektarbeit
+/usr/local/bin/app -port 8888 -token 123 -upload_limit 100000000 /home/Evaluation-of-WiFi-Infrastructure
